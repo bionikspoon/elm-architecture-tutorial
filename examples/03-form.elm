bcab27c3d82dc295d2664370f20bb9ff51b3ba1d
Manu Phatak <manu@thinkcerca.com>
2017-06-17 13:37:42 -0500

    add reset button

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -1,14 +1,18 @@
+module Main exposing (..)
+
+import String
+import Char
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
 
 main =
-  Html.beginnerProgram
-    { model = model
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
 
 
 
@@ -16,15 +20,16 @@ main =
 
 
 type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  }
+    { name : String
+    , password : String
+    , passwordAgain : String
+    , age : String
+    }
 
 
 model : Model
 model =
-  Model "" "" ""
+    Model "" "" "" ""
 
 
 
@@ -35,19 +40,23 @@ type Msg
     = Name String
     | Password String
     | PasswordAgain String
+    | Age String
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Name name ->
-      { model | name = name }
+    case msg of
+        Name name ->
+            { model | name = name }
 
-    Password password ->
-      { model | password = password }
+        Password password ->
+            { model | password = password }
 
-    PasswordAgain password ->
-      { model | passwordAgain = password }
+        PasswordAgain password ->
+            { model | passwordAgain = password }
+
+        Age age ->
+            { model | age = age }
 
 
 
@@ -56,21 +65,51 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ input [ type_ "text", placeholder "Name", onInput Name ] []
-    , input [ type_ "password", placeholder "Password", onInput Password ] []
-    , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-    , viewValidation model
-    ]
+    form [ onSubmit Submit ]
+        [ input [ type_ "text", placeholder "Name", onInput Name ] []
+        , input [ type_ "password", placeholder "Password", onInput Password ] []
+        , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+        , input [ type_ "number", placeholder "Age", onInput Age ] []
+        , viewValidation model
+        , button [] [ text "Submit" ]
+        ]
 
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  let
-    (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
-  in
-    div [ style [("color", color)] ] [ text message ]
+    let
+        ( color, message ) =
+            if String.length model.password < 8 then
+                ( "red", "Password must be longer than 8." )
+            else if not (contains model.password Char.isUpper) then
+                ( "red", "Password must contain an uppercase character." )
+            else if not (contains model.password Char.isLower) then
+                ( "red", "Password must contain an lowercase character." )
+            else if not (contains model.password Char.isDigit) then
+                ( "red", "Password must contain a digit character." )
+            else if not (model.password == model.passwordAgain) then
+                ( "red", "Passwords do not match!" )
+            else if not (isInt model.age) then
+                ( "red", "Age must be number." )
+            else
+                ( "green", "OK" )
+    in
+        div [ style [ ( "color", color ) ] ] [ text message ]
+
+
+contains : String -> (Char -> Bool) -> Bool
+contains password fn =
+    password
+        |> String.filter fn
+        |> String.length
+        |> (<) 0
+
+
+isInt : String -> Bool
+isInt number =
+    case String.toInt number of
+        Err _ ->
+            False
+
+        Ok _ ->
+            True
