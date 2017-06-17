e2a5dfd41f7e72fba1c17adb895c465ff5a25ab8
Manu Phatak <manu@thinkcerca.com>
2017-06-17 18:32:41 -0500

    add hours and seconds to clock

--- examples/06-clock.elm
+++ examples/06-clock.elm
@@ -1,29 +1,31 @@
+module Main exposing (..)
+
 import Html exposing (Html)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
 
 
-
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
 -- MODEL
 
 
-type alias Model = Time
+type alias Model =
+    Time
 
 
-init : (Model, Cmd Msg)
+init : ( Model, Cmd Msg )
 init =
-  (0, Cmd.none)
+    ( 0, Cmd.none )
 
 
 
@@ -31,14 +33,14 @@ init =
 
 
 type Msg
-  = Tick Time
+    = Tick Time
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Tick newTime ->
-      (newTime, Cmd.none)
+    case msg of
+        Tick newTime ->
+            ( newTime, Cmd.none )
 
 
 
@@ -47,7 +49,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second Tick
+    Time.every second Tick
 
 
 
@@ -56,17 +58,37 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  let
-    angle =
-      turns (Time.inMinutes model)
+    let
+        minuteAngle =
+            turns (Time.inMinutes model)
 
-    handX =
-      toString (50 + 40 * cos angle)
+        minuteHandX =
+            toString (50 + 40 * cos minuteAngle)
 
-    handY =
-      toString (50 + 40 * sin angle)
-  in
-    svg [ viewBox "0 0 100 100", width "300px" ]
-      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
-      ]
+        minuteHandY =
+            toString (50 + 40 * sin minuteAngle)
+
+        hourAngle =
+            turns (Time.inHours model)
+
+        hourHandX =
+            toString (50 + 20 * cos hourAngle)
+
+        hourHandY =
+            toString (50 + 20 * sin hourAngle)
+
+        secondAngle =
+            turns (Time.inSeconds model)
+
+        secondHandX =
+            toString (50 + 40 * cos secondAngle)
+
+        secondHandY =
+            toString (50 + 40 * sin secondAngle)
+    in
+        svg [ viewBox "0 0 100 100", width "300px" ]
+            [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+            , line [ x1 "50", y1 "50", x2 minuteHandX, y2 minuteHandY, stroke "#023963" ] []
+            , line [ x1 "50", y1 "50", x2 hourHandX, y2 hourHandY, stroke "#023963" ] []
+            , line [ x1 "50", y1 "50", x2 secondHandX, y2 secondHandY, stroke "red" ] []
+            ]
